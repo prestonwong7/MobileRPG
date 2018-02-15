a581419365ac3647acd41fb08314ef1356320c4d
preston <prestonwong7@gmail.com>
2018-02-15 10:39:58 -0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+#Testing File, test.py
+
+print("Hey guys, this is a testing file, feel free to change anything")
+print("Try using master branches, and changing different branches")
+print("Merge")
+print("Pull, push, commit!")
+
