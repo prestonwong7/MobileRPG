d8ab307afb68fa1f8f549570af5f241f9465bbbc
Ken Le <kle9597@gmail.com>
2018-02-20 13:22:13 -0800

    Get rekt m8

--- MobileRPG.py
+++ MobileRPG.py
@@ -1,26 +1,7 @@
-#Testing File, MobileRPG.py
+#we need to learn how to use github for unity
+#I think it is an extension to Unity and does not require the use of command line prompt
 
-print("MOBILE RPG GOGO")
+githubUnity = 9000
 
-
-print("Spicy")
-
-<<<<<<< HEAD
-print( "NOT SPICY")
-print("TEST CHANGE")
-
-print("TESTING CAHNAGSEGSE ADFSA F")
-=======
-
-for i in range(0,10):
-    print("I like spicy memes")
-
-print("spicyyy")
-
-spicy = 90000000000
-if(spicy > 1):
-    print("Too spicy for me)
-
-print("TEST CHANGE")
-
->>>>>>> 6afd8b401a0e5788e9752ca60f2a6167c0ce963b
+for i in range(9000):
+    print(githubUnity)
