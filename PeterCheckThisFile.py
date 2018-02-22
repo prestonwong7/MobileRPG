0d3f4c3f405fbec834667918d4ae137209718e4c
preston <prestonwong7@gmail.com>
2018-02-21 17:58:00 -0800

    Created new file for peter to look at

---
+++ PeterCheckThisFile.py
@@ -0,0 +1,6 @@
+#TESTING FOR PETER TO CHECK
+print("Creating pull request for peter. ")
+print("Peter after you look at this code, decide if this code needs to be changed or not")
+print("Then message me. If no changes, merge the pull request!")
+print("Testing!")
+#Mr. Peter
