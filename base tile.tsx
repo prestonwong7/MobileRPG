f64f058a72ec00bf2acdd610cdb3082316baa407
preston <prestonwong7@gmail.com>
2018-02-26 19:02:30 -0800

    Added my Unity Project

---
+++ base tile.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base tile" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../UnityPics/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+</tileset>
