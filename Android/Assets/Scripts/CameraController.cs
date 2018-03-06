0c8f34d3049d8b990d7eca41fa6c694b4df458fd
preston <prestonwong7@gmail.com>
2018-03-05 17:57:41 -0800

    Made new map, added boundaries, added new scene, added more code

--- Android/Assets/Scripts/CameraController.cs
+++ Android/Assets/Scripts/CameraController.cs
@@ -8,13 +8,26 @@ public class CameraController : MonoBehaviour {
     private Vector3 targetPos;
     public float moveSpeed;
 
+    private static bool cameraExists;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        DontDestroyOnLoad(transform.gameObject);
+        
+        if (!cameraExists)
+        {
+            cameraExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed*Time.deltaTime);
 	}
--- Android/Assets/Scripts/PlayerController.cs
+++ Android/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+
     private Animator anim;
+    private Rigidbody2D myRigidBody;
+
     private bool playerMoving;
     private Vector2 lastMove;
-    private int tester;
-    private int test2;
-    private int test3;
 
+    private static bool playerExists;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
 	}
 	
 	// Update is called once per frame
@@ -25,17 +38,30 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed , myRigidBody.velocity.y);
             playerMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         else if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
 
         }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f);
+        }
+
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
