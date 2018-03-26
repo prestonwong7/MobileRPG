00cee9f57b204725557a2cc1bded153b8b1b24ff
prestonwong7 <prestonwong7@gmail.com>
2018-03-26 00:28:19 -0700

    Added attack animation

--- Android/Assets/Scripts/PlayerController.cs
+++ Android/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour {
 
     private static bool playerExists;
 
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
@@ -35,31 +39,50 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         playerMoving = false;
-
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) // Horizontal movement
+        if (!attacking)
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-            myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed , myRigidBody.velocity.y);
-            playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f); // Gets last move for animation
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) // Horizontal movement
+            {
+                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f); // Gets last move for animation
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) // Vertical movement
+            {
+                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical")); // Last move for animation
+
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y); // Move speed horizontal
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f); // MOve speed vertically
+            }
         }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) // Vertical movement
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
-            playerMoving = true;
-            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical")); // Last move for animation
-
+            attackTimeCounter = attackTime;
+            attacking = true;
+            myRigidBody.velocity = Vector2.zero;
+            anim.SetBool("Attack", true);
         }
-
-        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        if (attackTimeCounter > 0)
         {
-            myRigidBody.velocity = new Vector2(0f, myRigidBody.velocity.y); // Move speed horizontal
+            attackTimeCounter -= Time.deltaTime;
+            
         }
-
-        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        if (attackTimeCounter <= 0)
         {
-            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0f); // MOve speed vertically
+            attacking = false;
+            anim.SetBool("Attack", false);
         }
 
 
