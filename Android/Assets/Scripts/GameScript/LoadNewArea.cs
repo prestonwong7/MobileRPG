6263b2d4878c04e7890602de2ce88259f05a14f9
prestonwong7 <prestonwong7@gmail.com>
2018-03-28 16:13:49 -0700

    CAN now add multiple start points and end points, make sure the name matcches!

---
+++ Android/Assets/Scripts/GameScript/LoadNewArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewArea : MonoBehaviour {
+
+    public string loadArea;
+
+    public string exitPoint;
+
+    private PlayerController thePlayer;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player1")
+        {
+            SceneManager.LoadScene(loadArea);
+            thePlayer.startPoint = exitPoint;
+        }   
+    }
+}
--- Android/Assets/Scripts/GameScript/PlayerStartPoint.cs
+++ Android/Assets/Scripts/GameScript/PlayerStartPoint.cs
@@ -7,13 +7,24 @@ public class PlayerStartPoint : MonoBehaviour {
     private PlayerController thePlayer;
     private CameraController theCamera;
 
+    public Vector2 StartDirection;
+
+    public string pointName;
+
 	// Use this for initialization
 	void Start () {
-        thePlayer = FindObjectOfType<PlayerController>();
-        thePlayer.transform.position = transform.position;
 
-        theCamera = FindObjectOfType<CameraController>();
-        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        if (thePlayer.startPoint == pointName)
+        {
+            
+            thePlayer.transform.position = transform.position;
+            //thePlayer.lastMove = StartDirection;
+
+            theCamera = FindObjectOfType<CameraController>();
+            theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+        }
 	}
 	
 	// Update is called once per frame
