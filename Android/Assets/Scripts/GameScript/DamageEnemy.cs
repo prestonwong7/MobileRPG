e61a8d754905ede1edb4d4210158e2ac4fec43ce
prestonwong7 <prestonwong7@gmail.com>
2018-03-28 02:32:52 -0700

    Added damage numbers for UI, and new scripts

---
+++ Android/Assets/Scripts/GameScript/DamageEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemy : MonoBehaviour {
+
+    public int damage;
+    public GameObject damageParticle;
+    public Transform pointOfImpact;
+    public GameObject damageNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (other.gameObject != null)
+            {
+                other.gameObject.GetComponent<EnemyHealthManager>().damageEnemy(damage);
+                Instantiate(damageParticle, pointOfImpact.position, pointOfImpact.rotation);
+                var clone = (GameObject) Instantiate(damageNumber, pointOfImpact.position, Quaternion.Euler(Vector3.zero)); // I dont get quaternion either
+                
+               
+                clone.GetComponent<FloatingNumbers>().damageNumber = damage;
+                clone.transform.position = new Vector2(pointOfImpact.position.x, pointOfImpact.position.y);
+                print("point " + pointOfImpact.position);
+                print("Clone is" + clone.transform.position);
+
+            }
+        }
+    }
+
+}
