268868b9abc7979de1438d05cf31de847678b3ed
Ken Le <kle9597@gmail.com>
2018-03-28 19:34:34 -0700

    updated version with music

---
+++ Android/Assets/Scripts/GameScript/DontDestoryAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestoryAudio : MonoBehaviour
+{
+    static DontDestoryAudio instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+  
+}
