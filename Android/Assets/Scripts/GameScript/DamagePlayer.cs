1d212452825e481c0920ba138d4c4e00f0db86e9
prestonwong7 <prestonwong7@gmail.com>
2018-04-01 01:37:53 -0700

    Added flashing when damaged, still need to fix player getting damaged

--- Android/Assets/Scripts/GameScript/DamagePlayer.cs
+++ Android/Assets/Scripts/GameScript/DamagePlayer.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class DamagePlayer : MonoBehaviour {
 
     public int damage;
+    public GameObject damageNumber;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -21,7 +22,10 @@ public class DamagePlayer : MonoBehaviour {
         if (other.gameObject.name == "Player1")
         {
             other.gameObject.GetComponent<PlayerHealthManager>().damagePlayer(damage); // Gets function from PlayerHealthManager class, hurting player
-           
+
+            var clone = (GameObject)Instantiate(damageNumber, other.transform.position, Quaternion.Euler(Vector3.zero)); // I dont get quaternion either
+            clone.GetComponent<FloatingNumbers>().damageNumber = damage;
+            clone.transform.position = new Vector2(other.transform.position.x, other.transform.position.y);
         }
     }
 }
