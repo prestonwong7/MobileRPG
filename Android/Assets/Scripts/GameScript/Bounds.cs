f367951c3b19fc0d1b302c24436b70166cdd0ebd
prestonwong7 <prestonwong7@gmail.com>
2018-04-02 01:22:51 -0700

    Camera now does not get outside the map except for first map

---
+++ Android/Assets/Scripts/GameScript/Bounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour {
+
+    private BoxCollider2D bounds;
+    private CameraController theCamera;
+
+	// Use this for initialization
+	void Start () {
+        bounds.GetComponent<BoxCollider2D>();
+        theCamera = FindObjectOfType<CameraController>();
+        theCamera.SetBounds(bounds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
--- Android/Assets/Scripts/GameScript/CameraController.cs
+++ Android/Assets/Scripts/GameScript/CameraController.cs
@@ -10,6 +10,14 @@ public class CameraController : MonoBehaviour {
 
     private static bool cameraExists;
 
+    public BoxCollider2D boundBox; // Used for sticking the camera inside the map
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight; // Unity calculates the vertical camera size divided by 2, not the full size
+    private float halfWidth;
+
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(transform.gameObject); //Doesnt destroy camera when switching scenes
@@ -24,11 +32,42 @@ public class CameraController : MonoBehaviour {
             Destroy(gameObject); // Destroy
         }
 
+        minBounds = boundBox.bounds.min; // Sets the x min position for boxCollider of bounds
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+
     }
 
     // Update is called once per frame
     void Update () {
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z); // Follows player
         transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed*Time.deltaTime); // Follows player
-	}
+
+        if (boundBox != null)
+        {
+            float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth); // Clamps make it so a value doesnt go past its min/max
+            float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+            transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+        }
+
+        if (boundBox == null)
+        {
+            boundBox = FindObjectOfType<Bounds>().GetComponent<BoxCollider2D>();
+            minBounds = boundBox.bounds.min;
+            maxBounds = boundBox.bounds.max;
+
+        }
+       
+    }
+
+    public void SetBounds(BoxCollider2D newBounds)
+    {
+        boundBox = newBounds;
+
+        minBounds = boundBox.bounds.min; // Sets the x min position for boxCollider of bounds
+        maxBounds = boundBox.bounds.max;
+    }
 }
