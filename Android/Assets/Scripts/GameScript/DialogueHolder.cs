16e1c1910e34c50e18c20dc59819f3ece88fd086
prestonwong7 <prestonwong7@gmail.com>
2018-04-08 14:44:26 -0700

    Added basic NPC dialogue box

---
+++ Android/Assets/Scripts/GameScript/DialogueHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager dialogueManage;
+
+	// Use this for initialization
+	void Start () {
+        dialogueManage = FindObjectOfType<DialogueManager>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*
+     * Every moment the player stays inside the box
+     * We don't use OnTriggerEnter2D because it just happens ONCE
+     */
+    private void OnTriggerStay2D(Collider2D other) 
+    { 
+        if (other.gameObject.name == "Player1")
+        {
+            if (Input.GetKeyUp(KeyCode.Z))
+            {
+                dialogueManage.ShowBox(dialogue);
+            }
+        }
+    }
+}
---
+++ Android/Assets/Scripts/GameScript/DialogueManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+    public bool dialogueActive; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            dialogueBox.SetActive(false);
+        }
+        else
+        {
+
+        }
+	}
+
+    public void ShowBox(string dialogue)
+    {
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+        dialogueText.text = dialogue;
+    }
+}
