a0ef57ed236a0b50081a2cd4c9649280033d934c
prestonwong7 <prestonwong7@gmail.com>
2018-04-14 00:50:37 -0700

    Started making quests, added quest scripts

---
+++ Android/Assets/Scripts/GameScript/QuestManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuestManager : MonoBehaviour {
+
+    public QuestObject[] quests;
+    public bool[] questCompleted;
+
+    public DialogueManager theDialogueManager;
+
+	// Use this for initialization
+	void Start () {
+        questCompleted = new bool[quests.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowQuestText(string questText)
+    {
+        theDialogueManager.dialogueLines = new string[1];
+        theDialogueManager.dialogueLines[0] = questText;
+
+        theDialogueManager.currentLine = 0;
+        theDialogueManager.ShowDialogue();
+    }
+
+
+}
---
+++ Android/Assets/Scripts/GameScript/QuestObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour {
+
+    public int questNumber;
+
+    public QuestManager theQuestManager;
+
+    public string startText;
+    public string endText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartQuest()
+    {
+        theQuestManager.ShowQuestText(startText);
+    }
+
+    public void EndQuest()
+    {
+        theQuestManager.ShowQuestText(endText);
+        theQuestManager.questCompleted[questNumber] = true;
+        gameObject.SetActive(false);
+    }
+}
