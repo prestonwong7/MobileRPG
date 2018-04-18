19ead4b9ce527c0cd49162a4b86cbe98a0c369de
elijahramos <elijahpramos@gmail.com>
2018-04-18 01:00:12 -0700

    Added audio settings functionality and started character creation menu

---
+++ Android/Assets/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public GameObject audioManager;
+    public Slider volumeSlider;
+
+	public void SetVolume()
+    {
+        //shit doesn't work fam
+        //audioManager.GetComponent<AudioSource>().volume = volumeSlider.value;
+    }
+}
---
+++ Android/Assets/UI Elements/MenuScripts/SelectOnInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
