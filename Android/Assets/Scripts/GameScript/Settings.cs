017fc89b89c8b23a287ce5ae833ddfae599093ff
Preston Wong <prestonwong7@gmai>
2018-04-23 19:10:14 -0700

    Added settings menu, made volume work

---
+++ Android/Assets/Scripts/GameScript/Settings.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Settings : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    //[HideInInspector]
+    //public bool settingsPressed;
+    public bool settingsActive;
+    private UIManager theUI;
+    public GameObject settingsMenu;
+    public bool exitActive;
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //Input.GetKeyUp(pressed);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print(eventData);
+        settingsActive = true;
+        settingsMenu.SetActive(true);
+        if (exitActive)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        theUI = FindObjectOfType<UIManager>();
+        //settingsMenu = FindObjectOfType<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
