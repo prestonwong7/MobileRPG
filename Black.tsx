b3ee45bdb53f119e37b272e35029e81ed24aaddd
prestonwong7 <prestonwong7@gmail.com>
2018-04-27 00:27:20 -0700

    Testing to fix black lines

--- Black.tsx
+++ Black.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Black" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
- <image source="../pixil-frame-0.png" width="16" height="16"/>
+ <image source="pixil-frame-0.png" width="16" height="16"/>
 </tileset>
--- base tile.tsx
+++ base tile.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="../UnityPics/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <image source="roguelikeSheet_transparent.png" width="968" height="526"/>
  <terraintypes>
   <terrain name="BLACK" tile="0"/>
  </terraintypes>
