e25e1c458b859712e4be31788956ed0305863f85
Preston Wong <prestonwong7@gmai>
2018-05-02 12:55:43 -0700

    Made new areas on Main map, made heal area, can now talk to NPCs and signs

---
+++ Android/Assets/Scripts/GameScript/HealArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealArea : MonoBehaviour {
+
+    private PlayerHealthManager thePHM;
+
+	// Use this for initialization
+	void Start () {
+        thePHM = FindObjectOfType<PlayerHealthManager>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            thePHM.playerCurrentHealth = thePHM.playerMaxHealth;
+        }
+    }
+}
