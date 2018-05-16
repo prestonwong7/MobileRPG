ea33e86c327e8464b1a30c05fc177a6ae54bc5be
elijahramos <elijahpramos@gmail.com>
2018-05-16 11:29:20 -0700

    Adding gender creation functionality

--- Android/Assets/Scripts/GameScript/PlayerController.cs
+++ Android/Assets/Scripts/GameScript/PlayerController.cs
@@ -215,4 +215,5 @@ public class PlayerController : MonoBehaviour
         
 
     }
+
 }
---
+++ Android/Assets/changeObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeObject : MonoBehaviour {
+    public GameObject male;
+    public GameObject female;
+    public Dropdown myDropdown;
+
+	// Use this for initialization
+	void Start () {
+        male.SetActive(true);
+        female.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update (Dropdown target) {
+        
+		if(target.value == 0)
+        {
+            male.SetActive(true);
+            female.SetActive(false);
+        }
+        if(target.value == 1)
+        {
+            male.SetActive(false);
+            female.SetActive(true);
+        }
+	}
+}
